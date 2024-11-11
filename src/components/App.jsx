3a77671c69088bb60219640f0485eb3c71f9a31a
Omar <omarsuleiman739@gmail.com>
2024-11-11 18:22:53 +0200

    Add h2 and p tags in Note.jsx then import it to the App.jsx to be displayed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Form from "./Form";
+import Note from "./Note";
 
 function App(){
     
     return (<div><Header />
     <h1>Hello There !!!</h1>
     <Form />
+    <Note />
     <Footer />
     </div>);
 }
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+    return (<div>
+        <h2>Note title</h2>
+        <p>Note description</p>
+    </div>);
+}
+
+export default Note;
