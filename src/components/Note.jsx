b84efb74d683b8516f9a7610e05d68a972ec40fc
Omar <omarsuleiman739@gmail.com>
2024-11-16 17:54:50 +0200

    Add handleClick function with props to delete note in Note component

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 function Note(props){
 
-    
+    function handleClick(){
+        props.deleteNote(props.id);
+    }
     return (<div>
         <h2>{props.title}</h2>
         <p>{props.content}</p>
-        <button>Delete Note</button>
+        <button onClick={handleClick}>Delete Note</button>
     </div>);
 }
 
