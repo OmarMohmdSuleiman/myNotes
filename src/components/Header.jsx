6a755c1b200de9165c55c178762c1fa8286eb1f1
Omar <omarsuleiman739@gmail.com>
2024-11-16 18:21:13 +0200

    Add an h3 in the Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 function Header(){
     return (
-        <div><h1 id="h1">This is HEADER</h1></div>
+        <div><h1 id="h1">This is HEADER</h1>
+        <h3>Enter the Note:</h3></div>
     );
 }
 export default Header;
