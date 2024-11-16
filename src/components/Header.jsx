dc8a0da78791069979ded99ba8877faba99df4fa
Omar <omarsuleiman739@gmail.com>
2024-11-16 18:18:53 +0200

    Center the div with root class in the center

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header(){
     return (
-        <div><h1>This is HEADER</h1></div>
+        <div><h1 id="h1">This is HEADER</h1></div>
     );
 }
 export default Header;
