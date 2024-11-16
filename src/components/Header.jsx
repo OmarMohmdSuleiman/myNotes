185d73aa3328483fa09f5150686e77f7e93fc9f1
Omar <omarsuleiman739@gmail.com>
2024-11-16 18:27:34 +0200

    Style the Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header(){
     return (
-        <div><h1 id="h1">This is HEADER</h1>
+        <div id="header"><h1 id="h1">This is HEADER</h1>
         <h3>Enter the Note:</h3></div>
     );
 }
